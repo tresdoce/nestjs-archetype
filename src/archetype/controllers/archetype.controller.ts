13969419e69aa30cc2344cf14a7e82d8b3986d27
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-02-14 14:04:09 -0300

    chore: add swagger to exclude manifest of documentation

--- src/archetype/controllers/archetype.controller.ts
+++ src/archetype/controllers/archetype.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ArchetypeService } from '../services/archetype.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class ArchetypeController {
   constructor(private readonly archetypeService: ArchetypeService) {}
 
   @Get('manifest')
+  @ApiExcludeEndpoint()
   async getArchetypeInfo() {
-    return await this.archetypeService.generateManifest();
+    return this.archetypeService.generateManifest();
   }
 }
